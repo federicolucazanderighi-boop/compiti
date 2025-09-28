c08d0e8c3db826b830c06956eb277379cb22c122
federicolucazanderighi-boop <federicoluca.zanderighi@liceoeinsteinmilano.edu.it>
2025-09-28 17:33:15 +0200

    Update esercizio11.py

--- esercizio11.py
+++ esercizio11.py
@@ -2,5 +2,5 @@ v1 = int(input("digita voto 1: "))
 v2 = int(input("digita voto 2: "))
 v3 = int(input("digita voto 3: "))
 media = ( v1 + v2 + v3 ) / 3
-print( f"La media è: {media}")
+print( "La media è: {media}")
 
