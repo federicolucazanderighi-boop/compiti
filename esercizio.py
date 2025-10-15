09b6c65c2e0ccd063a39d9ab116683d7c9faa7d3
federicolucazanderighi-boop <federicoluca.zanderighi@liceoeinsteinmilano.edu.it>
2025-10-15 19:39:59 +0200

    Add files via upload

---
+++ esercizio.py
@@ -0,0 +1,21 @@
+n = str(input("Inserisci il tuo nome: "))
+len(n)
+len (n) // 2
+if len (n) // 2 == 4:
+  a = n[0:3]
+  b = n[5:8]
+  print ("Il tuo nome è :",a + b)
+if len (n) / 2 == 3:
+  c = n[0:2]
+  d = n[4:6]
+  print ("Il tuo nome è: ",c + d)
+if len (n) / 2 == 2.5:
+  e = n[0:2]
+  f = n[3:5]
+  print ("Il tuo nome è :",e + f)
+if len (n) / 2 == 3.5:
+  a = n[0:3]
+  b = n[4:7]
+  print ("Il tuo nome è :",a + b)
+
+
